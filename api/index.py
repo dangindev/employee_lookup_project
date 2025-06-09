a6a31d29483b1109374b1e54925c5cf869e482f0
dangindev <18a10010365@students.hou.edu.vn>
2025-06-09 17:05:47 +0700

    Simplify Vercel configuration

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_lookup.settings')
 
-application = get_wsgi_application()
-application = WhiteNoise(application, root='staticfiles/') 
+application = get_wsgi_application() 
